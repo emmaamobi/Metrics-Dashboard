943ffa1c7a29c573ace56551a8311fb074e77bdf
eamobi <amobisomto@gmail.com>
2020-11-29 20:07:37 -0600

    insert calculations into db table

--- Issues/Code/sqlite_database.py
+++ Issues/Code/sqlite_database.py
@@ -39,6 +39,10 @@ This is some SQL code that creates the tables and columns in a database named af
 		comment_created_at VARCHAR(3000) ,
 		comment_updated_at VARCHAR(3000) ,
 		comment_body VARCHAR(3000)) ;''')
+    # Create table - ISSUES_CALCULATIONS
+        cursor.execute(
+            "CREATE TABLE IF NOT EXISTS ISSUES_CALCULATIONS (calc_name VARCHAR(3000), value VARCHAR(3000));"
+        )
 
 	# # Create table - MASTER
 	# cursor.execute("CREATE TABLE IF NOT EXISTS MASTER(date DATE, commits INT(3000), issues INT(3000), defect_density INT(3000), issue_spoilage_avg INT(3000), issue_spoilage_max INT(3000), issue_spoilage_min INT(3000), lines_of_code INT(300), PRIMARY KEY (date));")
